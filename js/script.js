05a0b97a1d9bbb98814b28f38b9df162bd375322
MattiaM99 <mattiamazzoni123@gmail.com>
2021-10-21 17:31:55 +0200

    fine parte senza bonus

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
-/* 1. chiedere l'età all
+/*
+1.chiedere all'utente quanti chilometri deve percorrere
+2. chiedere l'età all'utente
+3.sconto 20% under 18
+4.sconto 40% over 65
+*/
+
+const chilometri = parseInt(prompt('quanti chilometri devi percorrere?'));
+
+let prezzo = chilometri * 0.21;
+
+const scontoMinorenni = (prezzo / 100) * 20;
+
+const scontoAnziani = (prezzo / 100) * 40;
+
+let prezzoMinorenni = prezzo - scontoMinorenni;
+
+let prezzoAnziani = prezzo - scontoAnziani;
+
+const eta = parseInt(prompt('quanti anni hai?'));
+
+if (eta <= 18) {
+  document.getElementById('prezzo').innerHTML = '' + 'il tuo biglietto costa =' + '' + prezzoMinorenni + '€';
+ } else if (eta >= 65) {
+  document.getElementById('prezzo').innerHTML = '' + 'il tuo biglietto costa =' + '' + prezzoMinorenni + '€';
+ } else {
+  document.getElementById('prezzo').innerHTML = '' +  'il tuo biglietto costa='  + '' + prezzo + '€';
+ }
+
